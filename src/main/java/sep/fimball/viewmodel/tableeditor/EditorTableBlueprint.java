3b497a50fe86852c59e595b523af5e82cad06bdc
kairaedsch <kai.raedsch@mysticwind.de>
2016-11-03 01:43:57 +0100

    Editor stuff

--- src/main/java/sep/fimball/viewmodel/tableeditor/EditorTableBlueprint.java
+++ src/main/java/sep/fimball/viewmodel/tableeditor/EditorTableBlueprint.java
@@ -1,8 +1,22 @@
 package sep.fimball.viewmodel.tableeditor;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.awt.*;
+import java.util.List;
+
 /**
  * Created by kaira on 03.11.2016.
  */
 public class EditorTableBlueprint
 {
+    public EditorTableElementBlueprint getElementAt(Point p)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<EditorTableElementBlueprint> getElements(Point p1, Point p2)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/main/java/sep/fimball/viewmodel/tableeditor/EditorTableElementBlueprint.java
+++ src/main/java/sep/fimball/viewmodel/tableeditor/EditorTableElementBlueprint.java
@@ -1,8 +1,20 @@
 package sep.fimball.viewmodel.tableeditor;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.awt.*;
+import java.util.Map;
+
 /**
  * Created by kaira on 03.11.2016.
  */
 public class EditorTableElementBlueprint
 {
+    SimpleIntegerProperty blueprintId;
+    SimpleStringProperty greenprintId;
+    SimpleObjectProperty<Point> position;
+    SimpleObjectProperty<Map<String, Double>> colliderToMulitplier;
+    SimpleIntegerProperty points;
 }
