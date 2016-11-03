a3e91a077b045a90d35ba9c218ed99274b505623
alexcekay <admin@example.com>
2016-11-03 21:05:03 +0100

    added type to collisionargs

--- src/main/java/sep/fimball/model/CollisionEventArgs.java
+++ src/main/java/sep/fimball/model/CollisionEventArgs.java
@@ -3,18 +3,25 @@ package sep.fimball.model;
 public class CollisionEventArgs
 {
     private GameElement otherElement;
+    private Class otherElementType;
 
     /**
 	 *
 	 * @param otherElement
 	 */
-	public CollisionEventArgs(GameElement otherElement)
+	public CollisionEventArgs(GameElement otherElement, Class otherElementType)
     {
         this.otherElement = otherElement;
+        this.otherElementType = otherElementType;
     }
 
     public GameElement getOtherElement()
     {
         return otherElement;
+    }
+
+    public Class getOtherElementType()
+    {
+        return otherElementType;
     }
 }
