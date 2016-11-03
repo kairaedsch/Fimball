f95e55e604523368fc9ce658376f6871f4d17744
anonymous <anonymous>
2016-11-03 16:33:59 +0100

    PlayerNameEntryFxController added

---
+++ src/main/java/sep/fimball/view/dialog/playername/PlayerNameEntryFxController.java
@@ -0,0 +1,10 @@
+package sep.fimball.view.dialog.playername;
+
+import sep.fimball.view.dialog.Dialog;
+
+/**
+ * Created by kaira on 01.11.2016.
+ */
+public class PlayerNameEntryFxController
+{
+}
