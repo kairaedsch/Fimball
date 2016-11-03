1ea0952d909bf5d2a4b6e1d878cfe372a34546f9
tillwuebbers <till.wuebbers@gmail.com>
2016-11-03 21:38:50 +0100

    element greenprint

--- src/main/java/sep/fimball/model/elementgreenprint/ElementGreenprint.java
+++ src/main/java/sep/fimball/model/elementgreenprint/ElementGreenprint.java
@@ -1,8 +1,49 @@
 package sep.fimball.model.elementgreenprint;
 
+import sep.fimball.model.Vector2;
+
 /**
  * Created by kaira on 03.11.2016.
  */
 public class ElementGreenprint
 {
+    public class GreenprintCollider
+    {
+        public class AnimationObject
+        {
+            public int frameCount;
+            public double duration;
+            public String frameName;
+        }
+
+        public class ForceObject
+        {
+            public String forceType;
+            public double strength;
+        }
+
+        public class PhysicsObjectPolygon
+        {
+            public String type;
+            public boolean onGround;
+            public Vector2[] points;
+        }
+
+        public class PhysicsObjectCircle
+        {
+            public String type;
+            public boolean onGround;
+            public double x, y;
+            public double radius;
+        }
+
+        public String name;
+        public PhysicsObjectPolygon[] physicsPolygons;
+        public PhysicsObjectCircle[] physicsCircles;
+    }
+
+    public String editorName;
+    public String editorDescription;
+    public double rotationAccuracy;
+    public GreenprintCollider[] colliders;
 }
