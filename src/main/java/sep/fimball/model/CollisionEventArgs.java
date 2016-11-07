9deffd4d004ef42ad0c649568428ea92a8d4d506
tillwuebbers <till.wuebbers@gmail.com>
2016-11-08 00:02:59 +0100

    CollisionEventArgs

--- src/main/java/sep/fimball/model/CollisionEventArgs.java
+++ src/main/java/sep/fimball/model/CollisionEventArgs.java
@@ -1,22 +1,48 @@
 package sep.fimball.model;
 
+/**
+ * Argumente, die an das "Ziel-GameElement" bei einer Kollision mit einem anderen GameElement übergeben werden.
+ */
 public class CollisionEventArgs
 {
+    /**
+     * Das GameElement, das mit dem Ziel-GameElement zusammengestoßen ist.
+     */
     private GameElement otherElement;
-    private Collider collider;
 
     /**
-	 *
+     * Der Collider des Ziel-GameElements, der mit dem anderen GameElement zusammengestoßen ist.
+     */
+    private Collider ownCollider;
+
+    /**
+     * Der Collider des anderen GameElements, der mit dem Ziel-GameElement zusammengestoßen ist.
+     */
+    private Collider otherCollider;
+
+    /**
+	 * Erstellt eine neue Instanz von Kollisionsargumenten
 	 * @param otherElement
 	 */
-	public CollisionEventArgs(GameElement otherElement, Collider collider)
+	public CollisionEventArgs(GameElement otherElement, Collider ownCollider, Collider otherCollider)
     {
         this.otherElement = otherElement;
-        this.collider = collider;
+        this.ownCollider = ownCollider;
+        this.otherCollider = otherCollider;
     }
 
     public GameElement getOtherElement()
     {
         return otherElement;
+    }
+
+    public Collider getOwnCollider()
+    {
+        return ownCollider;
+    }
+
+    public Collider getOtherCollider()
+    {
+        return otherCollider;
     }
 }
