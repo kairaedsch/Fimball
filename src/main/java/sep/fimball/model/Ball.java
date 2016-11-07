582f3841276443942d98143b30a5934b212251ec
tillwuebbers <till.wuebbers@gmail.com>
2016-11-07 23:36:15 +0100

    Ball

--- src/main/java/sep/fimball/model/Ball.java
+++ src/main/java/sep/fimball/model/Ball.java
@@ -4,13 +4,24 @@ import sep.fimball.general.data.Vector2;
 import sep.fimball.model.blueprint.PlacedElement;
 
 /**
- * Created by alexcekay on 07.11.16.
+ * Repräsentiert einen Spielball auf dem Spielfeld eines Flipperautomaten.
  */
 public class Ball extends GameElement
 {
+    /**
+     * Geschwindigkeit des Balls auf dem Spielfeld, in Grid-Einheiten pro Sekunde auf der horizontalen bzw. vertikalen Achse.
+     */
     private Vector2 velocity;
-    private double angleAcceleration;
 
+    /**
+     * Geschwindigkeit der Rotation des Balls um die Achse, die aus dem Spielfeld "herausragt".
+     */
+    private double angularVelocity;
+
+    /**
+     * Erstellt einen neuen Ball.
+     * @param element
+     */
     public Ball(PlacedElement element)
     {
         super(element);
