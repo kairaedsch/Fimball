bb7e4ebb56504ffae526ca3f602f7d909d58ae87
tillwuebbers <till.wuebbers@gmail.com>
2016-11-07 23:27:11 +0100

    Animation

--- src/main/java/sep/fimball/model/Animation.java
+++ src/main/java/sep/fimball/model/Animation.java
@@ -4,22 +4,49 @@ import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
- * Created by theasuro on 07.11.16.
+ * Enthält die Pfade zu den Bild-Dateien einer Animation, und stellt das aktuell angezeigte Bild der Animation bereit.
  */
 public class Animation
 {
+    /**
+     * Array mit den Pfaden der einzelnen Bilder der Animation.
+     */
     private String[] framePaths;
+
+    /**
+     * Wie lang ein einzelnes Bild angezeigt wird, bevor zum nächsten gewechselt wird. In Sekunden.
+     */
     private double duration;
+
+    /**
+     * Property, an die vom SpriteViewModel gebunden wird. Diese enthält den Pfad zum aktuellen Bild.
+     */
     private StringProperty currentFrame;
 
+    /**
+     * Erstellt eine neue Animation.
+     *
+     * @param directoryPath
+     */
     public Animation(String directoryPath)
     {
         // load frames and stuff
     }
 
+    /**
+     * Wird kurz vor dem Zeichen des Bildes aufgerufen, um das aktuell ausgewählte Bild zu aktualisieren.
+     */
     public void update()
     {
         // move to next frame logic etc.
+    }
+
+    /**
+     * Setzt die Animation auf das erste Bild zurück.
+     */
+    public void startAnimation()
+    {
+
     }
 
     public ReadOnlyStringProperty currentFrameProperty()
