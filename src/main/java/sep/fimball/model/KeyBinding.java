0113c9c768611e1a09cc74696976c69e70fec043
tillwuebbers <till.wuebbers@gmail.com>
2016-11-08 02:03:22 +0100

    except i wasn't laughing

--- src/main/java/sep/fimball/model/KeyBinding.java
+++ src/main/java/sep/fimball/model/KeyBinding.java
@@ -1,12 +1,36 @@
 package sep.fimball.model;
 
+/**
+ * Enthält eine Liste von Funktionen, die von einem Tastendruck des Spielers ausgelöst werden können. Welche Taste diese auslöst kann in den Settings festgelegt werden.
+ */
 public enum KeyBinding
 {
+    /**
+     * Bewegt den linken Flipper beim Drücken der Taste nach oben, beim loslassen kehrt dieser in die Ausgangsposition zurück.
+     */
     LEFT_FLIPPER,
+    /**
+     * Bewegt den rechten Flipper beim Drücken der Taste nach oben, beim loslassen kehrt dieser in die Ausgangsposition zurück.
+     */
     RIGHT_FLIPPER,
+    /**
+     * Pausiert das Spiel. Falls dieses bereits pausiert ist, wird es fortgesetzt.
+     */
     PAUSE,
+    /**
+     * Stößt den Tisch etwas nach links. Kann bei zu häufiger Benutzung die Tilt-Funktion auslösen.
+     */
     NUDGE_LEFT,
+    /**
+     * Stößt den Tisch etwas nach rechts. Kann bei zu häufiger Benutzung die Tilt-Funktion auslösen.
+     */
     NUDGE_RIGHT,
+    /**
+     * Rotiert das ausgewählte Element im Editor um den im Element vorgegebenen Winkel nach rechts.
+     */
     EDITOR_ROTATE,
+    /**
+     * Löscht das im Editor ausgewählte Element.
+     */
     EDITOR_DELETE
 }
