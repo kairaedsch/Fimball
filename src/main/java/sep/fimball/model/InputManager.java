96070b963a3365869ba9ea61b59a1b80fe418bbb
tillwuebbers <till.wuebbers@gmail.com>
2016-11-08 01:57:33 +0100

    oh how we laughed and laughed

--- src/main/java/sep/fimball/model/InputManager.java
+++ src/main/java/sep/fimball/model/InputManager.java
@@ -3,10 +3,17 @@ package sep.fimball.model;
 import javafx.scene.input.KeyEvent;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Kontrolliert die Eingaben, die der Spieler über die Tastatur auslöst.
+ */
 public class InputManager
 {
+    /**
+     * Referenz auf die einzige Instanz des Managers. Teil des Singleton-Patterns. Das Pattern wird benötigt, da es keinen Sinn macht mehr als einen InputManager zu haben, und dieser von vielen Klassen des Models zugänglich sein muss.
+     */
     private static InputManager singletonInstance;
 
     public static InputManager getSingletonInstance()
@@ -17,18 +24,25 @@ public class InputManager
         return singletonInstance;
     }
 
-    private HashMap<KeyBinding, KeyObserver> keyObserversMap = new HashMap<>();
+    /**
+     * Weist jedem KeyBinding, welches bei einem in den Settings ausgewählten Tastendruck ausgelöst wird, eine Liste von Observern zu, welche bei diesem Tastendruck benachrichtigt werden.
+     */
+    private Map<KeyBinding, List<KeyObserver>> keyObserversMap = new HashMap<>();
 
     private InputManager()
     {
 
     }
 
+    /**
+     * Wenn in einer View ein Tastendruck ausgelöst wurde, wird dieser hier weitergeleitet, um mögliche Observer zu benachrichtigen.
+     * @param keyEvent
+     */
     public void addKeyEvent(KeyEvent keyEvent)
     {
         KeyBinding binding = Settings.getSingletonInstance().getKeyBindingsMap().get(keyEvent.getCode());
 
-        for (Map.Entry<KeyBinding, KeyObserver> mapEntry : keyObserversMap.entrySet())
+        for (Map.Entry<KeyBinding, List<KeyObserver>> mapEntry : keyObserversMap.entrySet())
         {
             if (mapEntry.getKey().equals(binding))
             {
@@ -41,17 +55,22 @@ public class InputManager
                 else
                     throw new IllegalArgumentException("Invalid keyEvent!");
 
-                mapEntry.getValue().keyEvent(new KeyObserverEventArgs(binding, state));
+                mapEntry.getValue().forEach((observer) -> observer.keyEvent(new KeyObserverEventArgs(binding, state)));
             }
         }
     }
 
+    /**
+     * Registriert einen neuen Observer, der bei einem bestimmten Tastendruck benachrichtigt wird.
+     * @param binding
+     * @param observer
+     */
     public void addListener(KeyBinding binding, KeyObserver observer)
     {
         if (observer == null)
         {
             throw new IllegalArgumentException("KeyObserver can't be null");
         }
-        keyObserversMap.put(binding, observer);
+        // TODO actually add to that ugly thing
     }
 }
