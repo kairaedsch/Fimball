f66ad1f3692e8d8cec89ac1b880759f25c571e97
tillwuebbers <till.wuebbers@gmail.com>
2016-11-08 02:05:20 +0100

    under the circumstances

--- src/main/java/sep/fimball/model/KeyObserver.java
+++ src/main/java/sep/fimball/model/KeyObserver.java
@@ -1,9 +1,13 @@
 package sep.fimball.model;
 
 /**
- * Created by alexcekay on 03.11.16.
+ * Dieses Interface wird benutzt, um beim InputManager auf einen bestimmten Tastendruck zu reagieren.
  */
 public interface KeyObserver
 {
+    /**
+     * Die Aktion, die beim Drücken/Loslassen der festgelegten Taste ausgeführt werden soll.
+     * @param args
+     */
     void keyEvent(KeyObserverEventArgs args);
 }
