4f93bd0c7aede65bdebf0d781b5e4d4574634591
tillwuebbers <till.wuebbers@gmail.com>
2016-11-08 02:07:34 +0100

    i've been shockingly nice

--- src/main/java/sep/fimball/model/KeyObserverEventArgs.java
+++ src/main/java/sep/fimball/model/KeyObserverEventArgs.java
@@ -1,19 +1,40 @@
 package sep.fimball.model;
 
 /**
- * Created by theasuro on 06.11.16.
+ * Die Argumente, die beim Auslösen eines Tastendruck-Events im InputManager übergeben werden.
  */
 public class KeyObserverEventArgs
 {
+    /**
+     * Status, zu dem die im InputManager festgelegte Taste gewechselt hat.
+     */
     public enum KeyChangedToState
     {
+        /**
+         * Die Taste wurde gedrückt.
+         */
         DOWN,
+        /**
+         * Die Taste wurde losgelassen.
+         */
         UP
     }
 
+    /**
+     * Die Funktion, die dieser Taste im InputManager zugeordnet ist.
+     */
     private KeyBinding binding;
+
+    /**
+     * Status, zu dem die im InputManager festgelegte Taste gewechselt hat.
+     */
     private KeyChangedToState state;
 
+    /**
+     * Erstellt eine neue Instanz der Klasse.
+     * @param binding
+     * @param state
+     */
     public KeyObserverEventArgs(KeyBinding binding, KeyChangedToState state)
     {
         this.binding = binding;
