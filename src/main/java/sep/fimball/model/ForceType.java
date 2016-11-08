027c4ed7eb4bc13945166cccdbcfa1e74a9dc80c
tillwuebbers <till.wuebbers@gmail.com>
2016-11-08 01:06:31 +0100

    it's always such a pleasure

--- src/main/java/sep/fimball/model/ForceType.java
+++ src/main/java/sep/fimball/model/ForceType.java
@@ -1,12 +1,24 @@
 package sep.fimball.model;
 
 /**
- * Created by alexcekay on 03.11.16.
+ * Enthält verschiedene Arten von Physik-Kräften, die beim aufprallen eines Balls an einem Collider auf den Ball gewirkt werden können.
  */
 public enum ForceType
 {
-    NORMAL, // just a wall
-    BOUNCE, // bounces ball (e.g. Bumper)
-    ACCELERATE, // accelerate thingy
+    /**
+     * Ball prallt normal an einer Wand ab, ohne besondere Beschleunigung.
+     */
+    NORMAL,
+    /**
+     * Stößt den Ball zusätzlich ab, wenn er an den Collider prallt, wie z.B. bei einem Bumper.
+     */
+    BOUNCE,
+    /**
+     * Beschleunigt den Ball bei Berührung. Der Ball prallt nicht am Collider ab.
+     */
+    ACCELERATE,
+    /**
+     * So lange der Ball diesen Collider berührt, befindet er sich auf einer Rampe. Der Ball prallt nicht am Collider ab.
+     */
     RAMP_FLOOR // keeps ball on top of ramp
 }
