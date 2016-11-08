54bf4e788f31060376a596f569082535b6205b76
kairaedsch <kai.raedsch@mysticwind.de>
2016-11-08 02:06:23 +0100

    Viewmodel Comments :(((((((((((((((((((( Nummer 4

--- src/main/java/sep/fimball/viewmodel/window/game/GameViewModel.java
+++ src/main/java/sep/fimball/viewmodel/window/game/GameViewModel.java
@@ -8,18 +8,44 @@ import sep.fimball.viewmodel.window.WindowType;
 import sep.fimball.viewmodel.window.WindowViewModel;
 
 /**
- * Created by TheAsuro on 04.11.2016.
+ * Das GameViewModel stellt der View Daten über eine aktuelle Partie Pinball zu Verfügung (Besonders über den aktiven Spieler).
  */
 public class GameViewModel extends WindowViewModel
 {
+    /**
+     * Die aktuelle Punktzahl(Score) des aktiven Spielers.
+     */
     private IntegerProperty playerPoints;
+
+    /**
+     * Der Name des aktiven Spielers.
+     */
     private StringProperty playerName;
+
+    /**
+     * Die Anzahl der Reservebälle des aktiven Spielers.
+     */
     private IntegerProperty playerReserveBalls;
+
+    /**
+     * Die aktuelle Position der Kamera, welche seinen Wert immer an die Kamera Position in PinballCanvasViewModel sendet. Dies geschieht durch Propertie Binding.
+     */
     private ObjectProperty<Vector2> cameraPosition;
+
+    /**
+     * Die aktuelle Zoom Stärke der Kamera, welche seinen Wert immer an die Kamera Zoom Stärke in PinballCanvasViewModel sendet. Dies geschieht durch Propertie Binding.
+     */
     private DoubleProperty cameraZoom;
 
+    /**
+     * Das PinballCanvasViewModel welches vom GameViewModel initialisiert/gesteuert wird.
+     */
     private PinballCanvasViewModel pinballCanvasViewModel;
 
+    /**
+     * Erstellt ein neues GameViewModel.
+     * @param gameSession
+     */
     public GameViewModel(GameSession gameSession)
     {
         super(WindowType.GAME);
@@ -40,33 +66,57 @@ public class GameViewModel extends WindowViewModel
         gameSession.startNewGame();
     }
 
+    /**
+     * Stellt die aktuelle Punktzahl(Score) für die View zu Verfügung.
+     * @return
+     */
     public IntegerProperty playerPointsProperty()
     {
         return playerPoints;
     }
 
+    /**
+     * Stellt den Name des aktiven Spielers für die View zu Verfügung.
+     * @return
+     */
     public StringProperty playerNameProperty()
     {
         return playerName;
     }
 
+    /**
+     * Stellt die Anzahl der Reservebälle des aktiven Spielers für die View zu Verfügung.
+     * @return
+     */
     public IntegerProperty playerReserveBallsProperty()
     {
         return playerReserveBalls;
     }
 
-    public PinballCanvasViewModel getPinballCanvasViewModel()
-    {
-        return pinballCanvasViewModel;
-    }
-
+    /**
+     * Stellt die aktuelle Position der Kamera für die View zu Verfügung.
+     * @return
+     */
     public ReadOnlyProperty<Vector2> cameraPositionProperty()
     {
         return cameraPosition;
     }
 
+    /**
+     * Stellt die aktuelle Zoom Stärke der Kamera für die View zu Verfügung.
+     * @return
+     */
     public ReadOnlyDoubleProperty cameraZoomProperty()
     {
         return cameraZoom;
+    }
+
+    /**
+     * Stellt das PinballCanvasViewModel für die View zu Verfügung, damit die View den Automaten mit all seinen Sprites zeichnen kann.
+     * @return
+     */
+    public PinballCanvasViewModel getPinballCanvasViewModel()
+    {
+        return pinballCanvasViewModel;
     }
 }
