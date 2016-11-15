7d48a6ded8726c3d3ab04295d086b76aefb03f0f
anonymous <anonymous>
2016-11-15 18:55:31 +0100

    added PinballMachineSaveTest

---
+++ src/main/java/sep/fimball/model/blueprint/PinballMachineSaveTest.java
@@ -0,0 +1,55 @@
+package sep.fimball.model.blueprint;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ReadOnlyMapProperty;
+import javafx.beans.property.StringProperty;
+import org.apache.xpath.operations.String;
+import org.junit.Test;
+import sep.fimball.general.data.Vector2;
+
+/**
+ *
+ */
+public class PinballMachineSaveTest
+{
+    private static final int MAX_ELEMENT_SIZE = 20;
+
+    @Test
+    public void pinballMachineShouldSave()
+    {
+        Integer pinballMachineId = PinballMachineManager.getInstance().createNew();
+        PinballMachine pinballMachine = PinballMachineManager.getInstance().tableBlueprintsProperty().valueAt(pinballMachineId).get();
+        String[] elementTypeIds;
+        ElementTypeManager.getInstance().elementsProperty().keySet().toArray(elementTypeIds);
+        PlacedElementList tableElementList = pinballMachine.getTableElementList();
+
+        for (int i = 0; i < elementTypeIds.length; i++)
+        {
+            tableElementList.addElement(i, new PlacedElement(elementTypeIds[i], new Vector2(0, MAX_ELEMENT_SIZE * i));
+        }
+
+        PinballMachineManager.getInstance().save(pinballMachineId);
+
+        //TODO load
+
+        PinballMachine loadedPinballMachine;
+
+        ReadOnlyMapProperty<Integer, PlacedElement> elementsPropertyOriginal = pinballMachine.getTableElementList().elementsProperty();
+        ReadOnlyMapProperty<Integer, PlacedElement> elementsPropertyLoaded = pinballMachine.getTableElementList().elementsProperty();
+
+        boolean difference = false;
+        for (Integer key : pinballMachine.getTableElementList().elementsProperty().keySet())
+        {
+            PlacedElement original = elementsPropertyOriginal.valueAt(key).get();
+            PlacedElement loaded = elementsPropertyLoaded.valueAt(key).get();
+
+            assert original != null;
+
+            if (!original.equals(loaded))
+            {
+                difference = true;
+            }
+        }
+
+    }
+}
