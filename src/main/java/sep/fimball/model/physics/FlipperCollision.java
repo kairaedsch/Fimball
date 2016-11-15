9446752761fb656cb274eea39e0522c26ed273ed
alexcekay <admin@example.com>
2016-11-15 22:03:22 +0100

    added new collision types

---
+++ src/main/java/sep/fimball/model/physics/FlipperCollision.java
@@ -0,0 +1,15 @@
+package sep.fimball.model.physics;
+
+import sep.fimball.general.data.Vector2;
+
+/**
+ * Created by alexcekay on 15.11.16.
+ */
+public class FlipperCollision implements CollisionType
+{
+    @Override
+    public void applyCollision(BallElement ball, Vector2 shortestIntersect)
+    {
+        
+    }
+}
---
+++ src/main/java/sep/fimball/model/physics/PlungerCollision.java
@@ -0,0 +1,16 @@
+package sep.fimball.model.physics;
+
+import sep.fimball.general.data.Vector2;
+
+/**
+ * Created by alexcekay on 15.11.16.
+ */
+public class PlungerCollision implements CollisionType
+{
+    @Override
+    public void applyCollision(BallElement ball, Vector2 shortestIntersect)
+    {
+        //TODO Apply collision
+        //TODO Increase y velocity
+    }
+}
