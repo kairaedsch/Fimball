d66c6a4f3f0906ea319cfa6c4997153a8d4fd9f3
Marc Schott <marc.schott94@gmail.com>
2016-11-16 18:04:28 +0100

    Anpassung der Sichtbarkeit in den Json-Klassen

--- src/main/java/sep/fimball/model/blueprint/json/PinballMachineJson.java
+++ src/main/java/sep/fimball/model/blueprint/json/PinballMachineJson.java
@@ -6,12 +6,12 @@ import sep.fimball.general.data.Highscore;
  * Created by marc on 16.11.16.
  */
 public class PinballMachineJson {
-    int tableId;
-    String name;
-    HighscoreJson[] highscores;
+    public int tableId;
+    public String name;
+    public HighscoreJson[] highscores;
 
     public static class HighscoreJson {
-        long score;
-        String playerName;
+        public long score;
+        public String playerName;
     }
 }
