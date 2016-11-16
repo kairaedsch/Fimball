5b2aff28cae5995e47b91a48988a26f69d5062d3
Marc Schott <marc.schott94@gmail.com>
2016-11-16 11:18:16 +0100

    Test ReserveBalls

---
+++ src/test/java/sep/fimball/model/ReserveBallsTest.java
@@ -0,0 +1,30 @@
+package sep.fimball.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import sep.fimball.model.blueprint.pinnballmachine.PinballMachine;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by marc on 16.11.16.
+ */
+public class ReserveBallsTest {
+    static String playerName = "tester";
+    static PinballMachine automaton;
+    static GameSession game;
+
+    @Before
+    public static void initGame() {
+        // TODO GameSession mit richtigem Automaten und drei ReserveBalls erstellen
+    }
+
+    @Test
+    public static void testReserveBalls() {
+        // TODO Mit Plunger einschießen
+        // TODO Warten bis Kugel weg oder onBallLost() aufrufen
+        // TODO Kugel mit Plunger einschießen
+        // TODO Warten bis Kugel weg oder onBallLost() aufrufen
+        assertEquals(1, game.getCurrentPlayer().getBalls());
+    }
+}
