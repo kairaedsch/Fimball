05a3169eecdde0b9c695cc0d57e02bf8b39f65e9
alexcekay <admin@example.com>
2016-11-21 12:04:49 +0100

    listproperty comments

--- src/main/java/sep/fimball/general/util/ListPropertyConverter.java
+++ src/main/java/sep/fimball/general/util/ListPropertyConverter.java
@@ -18,11 +18,11 @@ public class ListPropertyConverter
      * Löscht die Werte der listPropertySlave und füllt diese mit den Werten der listPropertyMaster, wenn sich letztere ändert.
      * TODO
      *
-     * @param listPropertySlave
-     * @param listPropertyMaster
-     * @param converter
-     * @param <SlaveT>
-     * @param <MasterT>
+     * @param listPropertySlave Die Liste in welcher die konvertierten Objekte gespeichert werden
+     * @param listPropertyMaster Die Liste welche bei Änderung in die Liste listPropertySlave konvertiert wird
+     * @param converter Ein Converter welcher angibt wie zwischen den Typen MasterT und SlaveT konvertiert wird
+     * @param <SlaveT> Der Typ der listPropertySlave Liste
+     * @param <MasterT> Der Typ der listPropertyMaster Liste
      */
     public static <SlaveT, MasterT> void bindAndConvertList(ListProperty<SlaveT> listPropertySlave, ObservableList<MasterT> listPropertyMaster, ListConverter<SlaveT, MasterT> converter)
     {
@@ -103,25 +103,42 @@ public class ListPropertyConverter
     }
 
     /**
-     * TODO
+     * Das Interface stellt einen allgemeinen Converter da welcher angegeben werden muss um aus einem Objekt vom Typ MasterT
+     * ein Objekt vom Typ SlaveT zu erstellen
      *
-     * @param <SlaveT>
-     * @param <MasterT>
+     * @param <SlaveT> Der Typ der listPropertySlave Liste
+     * @param <MasterT> Der Typ der listPropertyMaster Liste
      */
+    @FunctionalInterface
     public interface ListConverter<SlaveT, MasterT>
     {
+        /**
+         * Erstellt ein Objekt vom Typ SlaveT aus einem Objekt vom Typ MasterT
+         *
+         * @param master Das Objekt vom Typ MasterT aus dem ein SlaveT erstellt wird
+         * @return Ein Objekt vom Typ SlaveT
+         */
         SlaveT convert(MasterT master);
     }
 
     /**
-     * TODO
+     * Das Interface stellt einen allgemeinen Converter da welcher angegeben werden muss um aus einem Key/Value Paar vom Typ MasterT
+     * ein Objekt vom Typ SlaveT zu erzeugen
      *
      * @param <SlaveT>
      * @param <MasterKeyT>
      * @param <MasterValueT>
      */
+    @FunctionalInterface
     public interface MapConverter<SlaveT, MasterKeyT, MasterValueT>
     {
+        /**
+         * Erstellt ein Objekt vom Typ SlaveT aus einem Key/Value Paar vom Typ MasterT
+         *
+         * @param masterKey
+         * @param masterValueT
+         * @return Ein Objekt vom Typ SlaveT
+         */
         SlaveT convert(MasterKeyT masterKey, MasterValueT masterValueT);
     }
 }
