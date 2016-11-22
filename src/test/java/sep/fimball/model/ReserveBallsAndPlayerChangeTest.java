07d6f3647aa340b37f5268d6f7951fa4e90d6839
Marc Schott <marc.schott94@gmail.com>
2016-11-22 23:51:46 +0100

    wait und notify in ReserveBallAndPlayersChangeTest

--- src/test/java/sep/fimball/model/ReserveBallsAndPlayerChangeTest.java
+++ src/test/java/sep/fimball/model/ReserveBallsAndPlayerChangeTest.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import org.junit.After;
 import org.junit.Ignore;
 import org.junit.Test;
 import sep.fimball.model.blueprint.pinballmachine.PinballMachine;
@@ -26,63 +27,75 @@ public class ReserveBallsAndPlayerChangeTest {
     private static String[] players = new String[] {"tester", "test"};
     private PinballMachine automaton;
     private GameSession game;
-    private static final long waitingTime = 2000;
-    private static final long keyHoldingTime = 1000;
-    private volatile boolean ballLost = false;
+    private static final long WAITING_TIME = 2000;
+    private static final long KEY_HOLDING_TIME = 1000;
+    private static final long MAX_TEST_TIME = 120000;
+    private static Object monitor = new Object();
 
-    @Test(timeout = 12000)
-    public void testReserveBalls() {
+    @Test(timeout = MAX_TEST_TIME)
+    public void testReserveBalls() throws InterruptedException {
         new JFXPanel();
         Platform.runLater(()-> initGameSession());
         Platform.runLater(()->usePlunger());
-        while (!ballLost) { }
+        synchronized (monitor) {
+            monitor.wait(MAX_TEST_TIME);
+        }
         waitForRoundChange();
         assertEquals(3, game.getCurrentPlayer().ballsProperty().get());
         assertEquals("test", game.getCurrentPlayer().getName());
-        ballLost = false;
         Platform.runLater(()->usePlunger());
-        while (!ballLost) { }
+        synchronized (monitor) {
+            monitor.wait(MAX_TEST_TIME);
+        }
         waitForRoundChange();
         assertEquals(2, game.getCurrentPlayer().ballsProperty().get());
         assertEquals("tester", game.getCurrentPlayer().getName());
-        ballLost = false;
         Platform.runLater(()->usePlunger());
-        while (!ballLost) { }
+        synchronized (monitor) {
+            monitor.wait(MAX_TEST_TIME);
+        }
         waitForRoundChange();
         assertEquals(2, game.getCurrentPlayer().ballsProperty().get());
         assertEquals("test", game.getCurrentPlayer().getName());
+    }
+
+    @After
+    public void stopThreads () {
         Platform.runLater(()->{
             game.stopPhysics();
             game.stopTimeline();
         });
+        Platform.exit();
     }
 
     private void initGameSession() {
-        automaton = PinballMachineManager.getInstance().pinballMachinesProperty().stream().filter((PinballMachine machine)->machine.getID().equals("testmachine2")).findFirst().get();
+        automaton = PinballMachineManager.getInstance().pinballMachinesProperty().stream().filter((PinballMachine machine)->machine.getID().equals("0")).findFirst().get();
         game = new GameSession(automaton, players);
         List<Handler> triggers = HandlerFactory.generateAllHandlers(game);
         Handler ballLostChecker = new Handler();
         ballLostChecker.setGameHandler((GameEvent gameEvent)->{
             if(gameEvent == GameEvent.BALL_LOST) {
-                ballLost = true;
+                synchronized (monitor) {
+                    monitor.notify();
+                }
             }
         });
         triggers.add(ballLostChecker);
         game.setTriggers(triggers);
     }
 
-    private void waitForRoundChange() {
-        try {
-            Thread.sleep(waitingTime);
-        } catch (InterruptedException e) { }
+    private void waitForRoundChange() throws InterruptedException {
+        Thread.sleep(WAITING_TIME);
     }
 
     private void usePlunger() {
         KeyCode plungerKey = Settings.getSingletonInstance().keyBindingsMapProperty().get(KeyBinding.PLUNGER);
         InputManager.getSingletonInstance().addKeyEvent(new KeyEvent(KeyEvent.KEY_PRESSED, " ", plungerKey.name(), plungerKey, false, false, false, false));
         try {
-            Thread.sleep(keyHoldingTime);
-        } catch (InterruptedException e) { }
+            Thread.sleep(KEY_HOLDING_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         InputManager.getSingletonInstance().addKeyEvent(new KeyEvent(KeyEvent.KEY_RELEASED, " ", plungerKey.name(), plungerKey, false, false, false, false));
     }
 }
