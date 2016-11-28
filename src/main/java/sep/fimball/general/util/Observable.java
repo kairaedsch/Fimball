ba93bacd0b7d1caafc19ee4a9ae4fc7e159a8724
alexcekay <admin@example.com>
2016-11-28 13:33:30 -0500

    observable comments

--- src/main/java/sep/fimball/general/util/Observable.java
+++ src/main/java/sep/fimball/general/util/Observable.java
@@ -1,7 +1,8 @@
 package sep.fimball.general.util;
 
 /**
- * Ändert die Sichtbarkeit der Methode setChanged von protected auf public, um sie auch in Lambda-Ausdrücken nutzen zu können.
+ * Ändert die Sichtbarkeit der Methode setChanged von protected auf public. Dies wird benötigt um nicht unnötigerweise immer von Observable
+ * erben zu müssen. So kann einfach mit Lambda Ausdrücken bei Observer gearbeitet werden.
  */
 public class Observable extends java.util.Observable
 {
