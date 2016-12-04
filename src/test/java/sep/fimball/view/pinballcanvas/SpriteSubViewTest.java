ada0c2424464be5a461c22f3b4b41df8f86ad932
Marc Schott <marc.schott94@gmail.com>
2016-12-04 20:33:54 +0100

    Weiterarbeit am Test

--- src/test/java/sep/fimball/view/pinballcanvas/SpriteSubViewTest.java
+++ src/test/java/sep/fimball/view/pinballcanvas/SpriteSubViewTest.java
@@ -1,10 +1,21 @@
 package sep.fimball.view.pinballcanvas;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import sep.fimball.JavaFXThreadingRule;
 import sep.fimball.general.data.ImageLayer;
+import sep.fimball.view.tools.ImageCache;
 import sep.fimball.viewmodel.pinballcanvas.SpriteSubViewModel;
+
+import static org.mockito.ArgumentMatchers.anyString;
 
 /**
  * Created by marc on 04.12.16.
@@ -16,11 +27,17 @@ public class SpriteSubViewTest
     SpriteSubViewModel spriteSubViewModelMock;
     @Mock
     GraphicsContext graphicsContextMock;
+    @Mock
+    ImageCache imageCache;
+    @Mock
+    Image image;
 
     public void drawTest() {
-
+        ReadOnlyDoubleProperty rotation = new SimpleDoubleProperty();
+        Mockito.when(spriteSubViewModelMock.rotationProperty()).thenReturn(rotation);
+        Mockito.when(ImageCache.getInstance().getImage(anyString())).thenReturn(image);
 
         SpriteSubView spriteSubView = new SpriteSubView(spriteSubViewModelMock);
-        spriteSubView.draw(graphicsContextMock, ImageLayer.BOTTOM.TOP);
+        spriteSubView.draw(graphicsContextMock, ImageLayer.BOTTOM);
     }
 }
