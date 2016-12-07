2ebae1a796dba67efe41211bfd3f536fcf978ca9
tillwuebbers <till.wuebbers@gmail.com>
2016-12-07 12:02:20 +0100

    pointless test

---
+++ src/test/java/sep/fimball/viewmodel/dialog/KeybindSubViewModelTest.java
@@ -0,0 +1,39 @@
+package sep.fimball.viewmodel.dialog;
+
+import javafx.scene.input.KeyCode;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sep.fimball.model.blueprint.settings.Settings;
+import sep.fimball.model.input.data.KeyBinding;
+import sep.fimball.viewmodel.dialog.gamesettings.KeybindSubViewModel;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by TheAsuro on 07.12.2016.
+ */
+public class KeybindSubViewModelTest
+{
+    @Test
+    public void testKeyCodes()
+    {
+        String bindingName = "TEST";
+        Settings settings = mock(Settings.class);
+        KeyBinding binding = mock(KeyBinding.class);
+        Mockito.when(binding.getName()).thenReturn(bindingName);
+
+        // Test if code can handle null value
+        KeybindSubViewModel dialog1 = new KeybindSubViewModel(settings, binding, null);
+        dialog1.keyNameProperty();
+        assertThat(dialog1.keyNameProperty().get(), is("No Key set"));
+
+        // Test default behaviour
+        KeyCode testCode = KeyCode.A;
+        KeybindSubViewModel dialog2 = new KeybindSubViewModel(settings, binding, testCode);
+        dialog2.keyNameProperty();
+        assertThat(dialog2.keyNameProperty().get(), is(testCode.getName()));
+        assertThat(dialog2.elementNameProperty().get(), is(bindingName));
+    }
+}
