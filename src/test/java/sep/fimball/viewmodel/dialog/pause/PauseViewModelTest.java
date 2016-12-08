31c95953d743bc2cdfaeb6950635f4ef7d3a8840
alexcekay <admin@example.com>
2016-12-08 09:57:44 +0100

    javadoc

--- src/test/java/sep/fimball/viewmodel/dialog/pause/PauseViewModelTest.java
+++ src/test/java/sep/fimball/viewmodel/dialog/pause/PauseViewModelTest.java
@@ -15,22 +15,40 @@ import static org.mockito.Mockito.*;
  */
 public class PauseViewModelTest
 {
+    /**
+     * Testet ob das PauseViewModel korrekt auf Tastendrücke reagiert und das Spiel dann
+     * wieder aufnimmt.
+     */
     @Test
     public void testPauseViewModel()
     {
         KeyCode pauseKeyCode = KeyCode.ESCAPE;
+        KeyCode otherKeyCode = KeyCode.ENTER;
         GameViewModel gameViewModel = mock(GameViewModel.class);
         SceneManagerViewModel sceneManagerViewModel = mock(SceneManagerViewModel.class);
         Settings settings = mock(Settings.class);
-        when(settings.getKeyBinding(any())).thenReturn(KeyBinding.PAUSE);
+        when(settings.getKeyBinding(pauseKeyCode)).thenReturn(KeyBinding.PAUSE);
 
         PauseViewModel pauseViewModel = new PauseViewModel(gameViewModel, settings);
         pauseViewModel.setSceneManager(sceneManagerViewModel);
         //Das PauseViewModel dazu auffordern das Spiel wieder aufzunehmen.
         pauseViewModel.resumeGame();
         //Den Tastendruck simulieren der dazu führt dass das Spiel wieder aufgenommen wird.
-        pauseViewModel.handleKeyEvent(new KeyEvent(KeyEvent.KEY_PRESSED, "", pauseKeyCode.getName(), pauseKeyCode, false, false, false, false));
+        simulateKeyPressed(pauseViewModel, pauseKeyCode);
+        //Einen beliebigen anderen Tastendruck simulieren, darf keine Auswirkung haben.
+        simulateKeyPressed(pauseViewModel, otherKeyCode);
         //Überprüfen ob das ViewModel zweimal versucht hat das Spiel fortzusetzen.
         verify(gameViewModel, times(2)).resume();
+    }
+
+    /**
+     * Simuliert das Drücken einer Taste
+     *
+     * @param viewModel das ViewModel auf dem der Tastendruck simuliert werden soll.
+     * @param code der Tastendruck der simuliert werden soll.
+     */
+    private void simulateKeyPressed(PauseViewModel viewModel, KeyCode code)
+    {
+        viewModel.handleKeyEvent(new KeyEvent(KeyEvent.KEY_PRESSED, "", code.getName(), code, false, false, false, false));
     }
 }
