2e29b6393c8c32d07e2a9967c9676700f6b31b3f
kairaedsch <kai.raedsch@mysticwind.de>
2016-12-21 00:23:04 +0100

    PhysicsElement stuff

--- src/test/java/sep/fimball/model/physics/element/PhysicsElementTest.java
+++ src/test/java/sep/fimball/model/physics/element/PhysicsElementTest.java
@@ -3,11 +3,17 @@ package sep.fimball.model.physics.element;
 import org.junit.Test;
 import org.mockito.Mockito;
 import sep.fimball.general.data.Vector2;
+import sep.fimball.model.physics.collider.Collider;
+import sep.fimball.model.physics.game.CollisionEventArgs;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 public class PhysicsElementTest
@@ -16,9 +22,32 @@ public class PhysicsElementTest
     public void checkCollision() throws Exception
     {
         BasePhysicsElement basePhysicsElement = Mockito.mock(BasePhysicsElement.class);
-        when(basePhysicsElement.getColliders()).thenReturn(Collections.emptyList());
-        PhysicsElement physicsElement = new PhysicsElement<>(null, null, 0, basePhysicsElement);
+        Collider colliderA = Mockito.mock(Collider.class);
+        Collider colliderB = Mockito.mock(Collider.class);
+        List<Collider> colliders = new ArrayList<>();
+        colliders.add(colliderA);
+        colliders.add(colliderB);
+        when(basePhysicsElement.getColliders()).thenReturn(colliders);
+        PhysicsElement<Object> physicsElement = new PhysicsElement<>(null, null, 0, basePhysicsElement);
 
+        List<CollisionEventArgs<Object>> eventArgsList = new ArrayList<>();
+        BallPhysicsElement<Object> ballPhysicsElement = Mockito.mock(BallPhysicsElement.class);
+
+        when(colliderA.checkCollision(eq(ballPhysicsElement), any())).thenReturn(false);
+        when(colliderB.checkCollision(eq(ballPhysicsElement), any())).thenReturn(false);
+        physicsElement.checkCollision(eventArgsList, ballPhysicsElement);
+        assertThat(eventArgsList.size(), is(0));
+
+        when(colliderA.checkCollision(eq(ballPhysicsElement), any())).thenReturn(false);
+        when(colliderB.checkCollision(eq(ballPhysicsElement), any())).thenReturn(true);
+        physicsElement.checkCollision(eventArgsList, ballPhysicsElement);
+        assertThat(eventArgsList.size(), is(1));
+        eventArgsList.clear();
+
+        when(colliderA.checkCollision(eq(ballPhysicsElement), any())).thenReturn(true);
+        when(colliderB.checkCollision(eq(ballPhysicsElement), any())).thenReturn(true);
+        physicsElement.checkCollision(eventArgsList, ballPhysicsElement);
+        assertThat(eventArgsList.size(), is(2));
     }
 
     @Test
