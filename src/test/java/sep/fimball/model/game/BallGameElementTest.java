38d5fdecd1a9b6cffe6bb36d82e35531a0e538fe
Jennifer Bauer <jenny.rh@web.de>
2017-01-09 20:18:12 +0100

    BallGameElement and PlungerGameElement tests added

---
+++ src/test/java/sep/fimball/model/game/BallGameElementTest.java
@@ -0,0 +1,89 @@
+package sep.fimball.model.game;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import org.junit.Test;
+import sep.fimball.general.data.Vector2;
+import sep.fimball.model.blueprint.pinballmachine.PlacedElement;
+import sep.fimball.model.physics.element.BallNudgeModify;
+import sep.fimball.model.physics.element.BallPhysicsElement;
+import sep.fimball.model.physics.element.BallResetModify;
+import sep.fimball.model.physics.element.Modify;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * Testet die Klasse BallGameElement.
+ */
+public class BallGameElementTest
+{
+    /**
+     * Gibt an, ob das BallPhysicsElement nach links gestoßen wurde.
+     */
+    private boolean nudgeLeft = false;
+
+    /**
+     * Die Position, die das BallPhysicsElement nach dem Reset haben soll.
+     */
+    private Vector2 position = new Vector2(0,0);
+
+    /**
+     * Testet, ob das Stoßen an der Kugel an das BallPhysicElement weitergeben wird.
+     */
+    @Test
+    public void nudgeTest()
+    {
+        BallGameElement testBallGameElement = getTestBallGameElement();
+
+        testBallGameElement.nudge(true);
+        assertThat(nudgeLeft, is(true));
+        testBallGameElement.nudge(false);
+        assertThat(nudgeLeft,is(false));
+
+    }
+
+    /**
+     * Testet, ob das Zurücksetzen der Kugel an das BallPhysicElement weitergeben wird.
+     */
+    @Test
+    public void resetTest() {
+        BallGameElement testBallGameElement = getTestBallGameElement();
+
+        testBallGameElement.reset();
+        assertThat(position.getX(), is(2.0));
+        assertThat(position.getY(), is(2.0));
+
+    }
+
+    /**
+     * Erstellt ein Test-BallGameElement und gibt dieses zurück.
+     * @return Ein Test-BallGameElement.
+     */
+    private BallGameElement getTestBallGameElement()
+    {
+        PlacedElement element = mock(PlacedElement.class);
+        when(element.positionProperty()).thenReturn(new SimpleObjectProperty<>(new Vector2(2,2)));
+        when(element.rotationProperty()).thenReturn(new SimpleDoubleProperty());
+        when(element.pointsProperty()).thenReturn(new SimpleIntegerProperty());
+        BallPhysicsElement ballPhysicsElement = mock(BallPhysicsElement.class);
+        doAnswer(invocationOnMock ->
+        {
+            Modify modify = invocationOnMock.getArgument(0);
+            if (modify instanceof BallNudgeModify)
+            {
+                nudgeLeft = ((BallNudgeModify) modify).isLeft();
+            } else if (modify instanceof BallResetModify) {
+                position = ((BallResetModify) modify).getNewPosition();
+            }
+            return null;
+        }).when(ballPhysicsElement).addModify(any(Modify.class));
+
+        BallGameElement testBallGameElement = new BallGameElement(element, false);
+        testBallGameElement.setPhysicsElement(ballPhysicsElement);
+        return testBallGameElement;
+    }
+}
---
+++ src/test/java/sep/fimball/model/game/PlungerGameElementTest.java
@@ -0,0 +1,53 @@
+package sep.fimball.model.game;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import org.junit.Test;
+import sep.fimball.model.blueprint.pinballmachine.PlacedElement;
+import sep.fimball.model.input.data.KeyBinding;
+import sep.fimball.model.input.manager.KeyEventArgs;
+import sep.fimball.model.physics.element.Modify;
+import sep.fimball.model.physics.element.PlungerModify;
+import sep.fimball.model.physics.element.PlungerPhysicsElement;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * Testet die Klasse PlungerGameElement.
+ */
+public class PlungerGameElementTest
+{
+    /**
+     * Gibt an, ob eine Kraft an das PlungerPhysicsElement weitergegeben wurde.
+     */
+    private boolean plungerForceActivated = false;
+
+    /**
+     * Testet, ob nach dem Aufladen und Loslaassen des Plungers eine Kraft an das PlungerPhysicsElement weitergegeben wird.
+     */
+    @Test
+    public void activateUserHandlerTest() {
+        PlacedElement placedElement = mock(PlacedElement.class);
+        when(placedElement.positionProperty()).thenReturn(new SimpleObjectProperty<>());
+        when(placedElement.rotationProperty()).thenReturn(new SimpleDoubleProperty());
+        when(placedElement.pointsProperty()).thenReturn(new SimpleIntegerProperty());
+        PlungerPhysicsElement plungerPhysicsElement = mock(PlungerPhysicsElement.class);
+        doAnswer(invocationOnMock -> {
+            Modify modify = invocationOnMock.getArgument(0);
+            if(modify instanceof PlungerModify) {
+                plungerForceActivated = true;
+            }
+            return null;}).when(plungerPhysicsElement).addModify(any());
+        PlungerGameElement testPlungerGameElement = new PlungerGameElement(placedElement,false);
+        testPlungerGameElement.setPhysicsElement(plungerPhysicsElement);
+
+        testPlungerGameElement.activateUserHandler(new KeyEventArgs(KeyBinding.PLUNGER, KeyEventArgs.KeyChangedToState.DOWN, true));
+        testPlungerGameElement.activateUserHandler(new KeyEventArgs(KeyBinding.PLUNGER, KeyEventArgs.KeyChangedToState.UP, true));
+
+        assertThat(plungerForceActivated, is(true));
+    }
+}
