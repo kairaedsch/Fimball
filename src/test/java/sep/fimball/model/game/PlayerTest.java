414616710971999c0fc0f56098a0b807abece7ad
Jennifer Bauer <jenny.rh@web.de>
2017-01-10 15:36:10 +0100

    player test erweitert

--- src/test/java/sep/fimball/model/game/PlayerTest.java
+++ src/test/java/sep/fimball/model/game/PlayerTest.java
@@ -30,7 +30,7 @@ public class PlayerTest
     }
 
     /**
-     * Testet, ob Punkte richtig hinzugefügt/abgezogen werden
+     * Testet, ob Punkte richtig hinzugefügt/abgezogen werden.
      */
     @Test
     public void addPointsTest()
@@ -46,4 +46,21 @@ public class PlayerTest
         player.addPoints(-30);
         assertThat(player.pointsProperty().get(), is(173));
     }
+
+    /**
+     * Testet, ob zwei Spieler richtig verglichen werden.
+     */
+    @Test
+    public void equalTest() {
+        Player player1 = new Player("TestPlayer");
+        Player player2 = new Player("OtherTestPlayer");
+        Player player3 = new Player("TestPlayer");
+
+        assertThat(player1.equals(player2), is(false));
+        assertThat(player1.equals(player3), is(true));
+        player1.addPoints(5);
+        assertThat(player1.equals(player3), is(false));
+    }
+
+
 }
