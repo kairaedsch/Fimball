48027eb753de8529563d4bd4cd4d681a2ecfc878
alexcekay <admin@example.com>
2017-01-18 15:41:25 +0100

    elementfactorytest updated

--- src/test/java/sep/fimball/model/game/ElementFactoryTest.java
+++ src/test/java/sep/fimball/model/game/ElementFactoryTest.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -50,7 +50,7 @@ public class ElementFactoryTest
 
         assertThat("Es wurden 9 GameElemente generiert", generatedElements.getGameElements().size(), is(9));
         assertThat("Es wurden 7 PhysicsElemente generiert", generatedElements.getPhysicsElements().size(), is(7));
-        assertThat("Es wurde ein BallGameElement generiert", generatedElements.getBallGameElement(), is(not(null)));
+        assertThat("Es wurde ein BallGameElement generiert", generatedElements.getBallGameElement(), is(notNullValue()));
 
         for (GameElement gameElement : generatedElements.getGameElements())
         {
