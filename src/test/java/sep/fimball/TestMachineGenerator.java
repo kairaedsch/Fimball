238a54260c2a0935f3dc3fdf5183bbef031c1045
anonymous <anonymous>
2017-01-18 01:36:52 +0100

    small changes to machinegenerator

--- src/test/java/sep/fimball/TestMachineGenerator.java
+++ src/test/java/sep/fimball/TestMachineGenerator.java
@@ -3,17 +3,12 @@ package sep.fimball;
 import org.junit.Ignore;
 import org.junit.Test;
 import sep.fimball.general.data.Vector2;
-import sep.fimball.model.blueprint.base.BaseElement;
 import sep.fimball.model.blueprint.base.BaseElementManager;
 import sep.fimball.model.blueprint.pinballmachine.PinballMachine;
 import sep.fimball.model.blueprint.pinballmachine.PinballMachineManager;
 import sep.fimball.model.blueprint.pinballmachine.PlacedElement;
 
 import java.util.ArrayList;
-import java.util.PrimitiveIterator;
-import java.util.Random;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
 
 @Ignore
 public class TestMachineGenerator
@@ -21,47 +16,32 @@ public class TestMachineGenerator
     @Test
     public void generateScalabilityTestMachine()
     {
-        int[] sizes = {16, 32, 64, 128, 256, 512, 1024};
+        int[] sizes = {16, 32, 64, 128, 256, 512};
         final int distance = 20;
 
-        for (int i = 0; i < sizes.length; i++)
+        for (int size : sizes)
         {
             PinballMachine pinballMachine = PinballMachineManager.getInstance().createNewMachine();
 
-            int size = sizes[i];
-
             pinballMachine.nameProperty().setValue("Test - Scalability " + size + "*" + size);
 
-            String[] keys = new String[BaseElementManager.getInstance().elementsProperty().getSize()];
-            BaseElementManager.getInstance().elementsProperty().get().keySet().toArray(keys);
-
-            BaseElement[] baseElements = new BaseElement[keys.length];
-            for (int k = 0; k < keys.length; k++)
-            {
-                baseElements[k] = BaseElementManager.getInstance().getElement(keys[k]);
-            }
-
-            ArrayList<PlacedElement> list = new ArrayList<>();
-
-            PrimitiveIterator.OfInt iterator = new Random().doubles(size * size).mapToInt(d -> (int) (keys.length * d)).iterator();
-            int halfSize = size / 2;
             for (int x = 0; x < size; x++)
             {
-                int xPos = (x - halfSize) * distance;
                 for (int y = 0; y < size; y++)
                 {
-                    list.add(new PlacedElement(baseElements[iterator.next()], new Vector2(xPos, y * distance), 0, 1, 0));
+                    String[] keys = new String[BaseElementManager.getInstance().elementsProperty().getSize()];
+                    BaseElementManager.getInstance().elementsProperty().get().keySet().toArray(keys);
+                    int index = (int) Math.floor(keys.length * Math.random());
+                    String key = keys[index];
+
+                    if (!key.equals("ball"))
+                    {
+                        pinballMachine.addElement(BaseElementManager.getInstance().getElement(key), new Vector2(x * distance - (size * distance / 2), y * distance));
+                    }
                 }
             }
-            System.out.println("created list with: " + size);
-
-            pinballMachine.addElement(list.toArray(new PlacedElement[0]));
-
-            System.out.println("created machine with: " + size);
 
             pinballMachine.saveToDisk(true);
-
-            System.out.println("saved machine with: " + size);
         }
     }
 
@@ -91,20 +71,23 @@ public class TestMachineGenerator
     @Test
     public void generateScalabilityCollisionTestMachine()
     {
-        PinballMachine pinballMachine = PinballMachineManager.getInstance().createNewMachine();
+        int[] sizes = {32, 64, 128, 256, 512, 1024};
 
-        final int size = 512;
-
-        pinballMachine.nameProperty().setValue("Test - Scalability Collision " + size);
-
-        pinballMachine.addElement(new PlacedElement(BaseElementManager.getInstance().getElement("hinderniss_linie_2"), new Vector2(1, 8), 0, 0, 0));
-
-        for (int i = 0; i < size; i++)
+        for (int size : sizes)
         {
-            PlacedElement element = new PlacedElement(BaseElementManager.getInstance().getElement("target_green"), new Vector2(0, 6), 1, 0, 0);
-            pinballMachine.addElement(element);
-        }
+            PinballMachine pinballMachine = PinballMachineManager.getInstance().createNewMachine();
 
-        pinballMachine.saveToDisk(true);
+            pinballMachine.nameProperty().setValue("Test - Scalability Collision " + size);
+
+            pinballMachine.addElement(new PlacedElement(BaseElementManager.getInstance().getElement("hinderniss_linie_2"), new Vector2(1, 8), 0, 0, 0));
+
+            for (int i = 0; i < size; i++)
+            {
+                PlacedElement element = new PlacedElement(BaseElementManager.getInstance().getElement("speed"), new Vector2(0, 6), 1, 0, 0);
+                pinballMachine.addElement(element);
+            }
+
+            pinballMachine.saveToDisk(true);
+        }
     }
 }
