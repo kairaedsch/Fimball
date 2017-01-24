8d552803f8502b015228d870d685b1cb33972de4
Marc Schott <marc.schott94@gmail.com>
2017-01-24 18:32:17 +0100

    Verbesserung des LanguageManagerViewModelTests

--- src/test/java/sep/fimball/viewmodel/LanguageManagerViewModelTest.java
+++ src/test/java/sep/fimball/viewmodel/LanguageManagerViewModelTest.java
@@ -1,16 +1,8 @@
 package sep.fimball.viewmodel;
 
-import org.junit.ClassRule;
 import org.junit.Test;
-import sep.fimball.JavaFXThreadingRule;
-import sep.fimball.general.data.DataPath;
 import sep.fimball.general.data.Language;
 import sep.fimball.model.blueprint.settings.Settings;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertFalse;
@@ -22,61 +14,26 @@ import static org.junit.Assert.assertThat;
 public class LanguageManagerViewModelTest
 {
     /**
-     * Wird benötigt um eine korrekte Ausführung auf dem JavaFX Thread zu garantieren.
-     */
-    @ClassRule
-    public static JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
-
-    /**
      * Testet, ob das LanguageManagerViewModel die gewünschten Texte in der richtigen Sprache anzeigt.
      */
-    @Test
+    @Test (timeout = 1000)
     public void languageTest()
     {
+        Language originalLanguage = Settings.getSingletonInstance().languageProperty().get();
+        Settings.getSingletonInstance().languageProperty().set(Language.GERMAN);
         LanguageManagerViewModel languageManagerViewModel = LanguageManagerViewModel.getInstance();
 
-        //Testet, ob das LanguageManagerViewModel eine richtige Instanz zurück gibt.
         assertFalse(languageManagerViewModel == null);
 
-        for (Language language : Language.values())
-        {
-            Settings.getSingletonInstance().languageProperty().set(language);
+        assertThat(languageManagerViewModel.textProperty("editor.selected.remove.key").get(), equalTo("Löschen"));
+        assertThat(languageManagerViewModel.textProperty("mainmenu.play.tip.key").get(), equalTo("Automaten spielen"));
 
-            String path = DataPath.pathToLanguage(language.getCode());
+        Settings.getSingletonInstance().languageProperty().set(Language.ENGLISH);
 
-            //Lädt die Properties aus der Datei
-            Properties properties = loadProperties(path);
+        assertThat(languageManagerViewModel.textProperty("editor.selected.remove.key").get(), equalTo("Remove"));
+        assertThat(languageManagerViewModel.textProperty("gamesettings.general.display.fullscreen.tip.key").get(),
+                equalTo("Fullscreen-mode hides the frame\n and displays the game on the whole screen."));
 
-            //Testet, ob die Texte aller ausgelesenen Keys aus der Properties-Datei auch im LanguageManagerViewModel enthalten
-            // sind.
-            for (Object key : properties.keySet())
-            {
-                assertThat("LanguageManagerViewModel lädt den richtigen Text zum gegebenen Key in der richtigen Sprache", properties.get(key), equalTo(LanguageManagerViewModel.getInstance().textProperty((String) key).get()));
-            }
-        }
-
-    }
-
-    /**
-     * Lädt die Properties aus der in {@code path} angegebenen Datei.
-     *
-     * @param path Der Pfad zur Datei.
-     * @return Die geladenen Properties
-     */
-    private Properties loadProperties(String path)
-    {
-        Properties properties = new Properties();
-        try (InputStream inputStream = LanguageManagerViewModel.class.getClassLoader().getResourceAsStream(path))
-        {
-            Reader reader = new InputStreamReader(inputStream, "UTF-8");
-            properties.load(reader);
-            inputStream.close();
-        }
-        catch (Exception e)
-        {
-            System.err.println("property file '" + path + "' not loaded");
-            System.out.println("Exception: " + e);
-        }
-        return properties;
+        Settings.getSingletonInstance().languageProperty().set(originalLanguage);
     }
 }
